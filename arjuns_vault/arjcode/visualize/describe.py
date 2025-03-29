9cc357bfc60e6df73c6dec965f5f27e4ee3ec0a9
Arjun Agarwal <arjun.agarwal@qure.ai>
2025-03-29 05:42:22 +0000

    minor refactor of describe

--- arjuns_vault/arjcode/visualize/describe.py
+++ arjuns_vault/arjcode/visualize/describe.py
@@ -1,15 +1,34 @@
+import pandas as pd
 from prettytable import PrettyTable
 
 
 def describe_model(model):
-    total_params = 0
+    information = pd.DataFrame(columns=["Module"]).set_index("Module")
 
-    table = PrettyTable(["Module", "Parameters"])
     for module, parameters in model.named_parameters():
         if not parameters.requires_grad:
             continue
         params = parameters.numel()
-        table.add_row([module, f"{params:,}"])
-        total_params += params
-    print(f"Total Parameters: {total_params:,}")
+        information.loc[module, "Parameters"] = params
+
+    information.loc["TOTAL", "Parameters"] = information["Parameters"].sum()
+
+    information = information.reset_index()
+    information = information.astype({"Parameters": int})
+
+    description = information.iloc[:-1]
+    summary = information.iloc[-1:]
+
+    table = PrettyTable(information.columns.tolist())
+    table.add_rows(description.values.tolist())
+    table.add_divider()
+    table.add_rows(summary.values.tolist())
     print(table)
+
+
+if __name__ == "__main__":
+    from torch import nn
+
+    # Example usage
+    model = nn.Sequential(nn.Linear(10, 20), nn.ReLU(), nn.Linear(20, 10))
+    describe_model(model)
