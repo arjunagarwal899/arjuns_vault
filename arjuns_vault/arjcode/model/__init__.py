a097700bc0c8d0a3abb32628d0eba0a6337634b8
Arjun Agarwal <arjun.agarwal@qure.ai>
2025-04-03 13:09:46 +0000

    updated bashrc

--- arjuns_vault/arjcode/model/__init__.py
+++ arjuns_vault/arjcode/model/__init__.py
@@ -1,2 +1,3 @@
+from .environment import set_multi_node_environment
 from .my_lightning_module import MyLightningModule
 from .parameters import freeze_module, freeze_modules, unfreeze_module, unfreeze_modules
